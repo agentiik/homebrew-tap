421b9781323d0cd581f2c8998d08df45bd7a6607
François Rousselet <francois.rousselet@rslt.fr>
2026-09-26 14:17:57 +0200

    Build agk and agentiik from v0.2.2 (#8)

--- Formula/agk.rb
+++ Formula/agk.rb
@@ -10,8 +10,8 @@ class Agk < Formula
   desc "Command-line tool for Agentiik workflows: validate, graph and run them locally"
   homepage "https://agentiik.github.io/docs"
   url "https://github.com/agentiik/agentiik.git",
-      tag:      "v0.2.1",
-      revision: "95f6f889439e22900df8d9d42252247a5439e360"
+      tag:      "v0.2.2",
+      revision: "d3a58fe5b0ebca23c389821de54fc17bdcb11e6b"
   license "AGPL-3.0-or-later"
   head "https://github.com/agentiik/agentiik.git", branch: "main"
 
